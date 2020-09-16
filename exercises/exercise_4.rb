50efb3d9447df65085db7d57a7cc099b4ba774fa
derekb123 <derekbutvin@gmail.com>
2020-09-16 00:37:13 +0000

    active record query exercises

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,21 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel: false, womens_apparel: true)
+
+Store.create(name: "Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false)
+
+Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+for i in @mens_stores
+  puts ("name : #{i.name}, annual revenue: #{i.annual_revenue}")
+end
+
+@w_stores_less_1mil = Store.where("womens_apparel = true AND annual_revenue <= 1000000")
+
+for i in @w_stores_less_1mil
+  puts ("name : #{i.name}, annual revenue: #{i.annual_revenue}")
+end
