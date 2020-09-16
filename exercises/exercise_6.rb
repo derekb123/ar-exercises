66b8d3b9135773a3a97670ab53ac3e64e621f408
derekb123 <derekbutvin@gmail.com>
2020-09-16 01:34:28 +0000

    added relationships to library files and created new employees in employee table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 30)
+@store2.employees.create(first_name: "Albert", last_name: "Camus", hourly_rate: 20)
+@store2.employees.create(first_name: "Paul", last_name: "Wall", hourly_rate: 40)
+
